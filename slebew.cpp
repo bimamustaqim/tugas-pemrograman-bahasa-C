ecac08861bb9555ac897726f4885488a402b9a0b
bimamustaqim <bmustaqim0@gmail.com>
2024-09-25 21:41:55 +0700

    Add files via upload

---
+++ slebew.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+ {
+	
+	
+    char nama[] = "Achmad Bima Mustaqim";
+    char nim[] = "2441001";
+    char jurusan[] = "Teknik Informatika";
+    
+    printf("Nama: %s\n", nama);
+    printf("NIM: %s\n", nim);
+    printf("Jurusan: %s\n", jurusan);
+
+    return 0;
+}
+
